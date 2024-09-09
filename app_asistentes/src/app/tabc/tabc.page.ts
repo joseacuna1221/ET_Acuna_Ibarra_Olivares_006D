69d25950f8dd8669d2843c6eacadcdfc7ef95b77
joseacuna1221 <134325342+joseacuna1221@users.noreply.github.com>
2024-09-09 01:45:02 -0300

    Etapa 1: Aplicacion Asistentes

---
+++ app_asistentes/src/app/tabc/tabc.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabc',
+  templateUrl: './tabc.page.html',
+  styleUrls: ['./tabc.page.scss'],
+})
+export class TabcPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
